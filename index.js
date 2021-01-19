51267fc703a7c594b2cc476043d86b32f27df222
rayalva407 <rayalva407@gmail.com>
2021-01-18 20:50:17 -0700

    adds javascript for moving other hands

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const secondsHand = document.querySelector('.sec-hand')
+const minutesHand = document.querySelector('.min-hand')
+const hoursHand = document.querySelector('.hour-hand')
 
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
 
   const secondsDeg = ((seconds / 60) * 360) + 90;
+  const minutesDeg = ((minutes / 60) * 360) + 90;
+  const hoursDeg = ((hours / 12) * 360) + 90;
 
   secondsHand.style.transform = `rotate(${secondsDeg}deg)`
+  minutesHand.style.transform = `rotate(${minutesDeg}deg)`
+  hoursHand.style.transform = `rotate(${hoursDeg}deg)`
 
 };
 
