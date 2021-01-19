6bb5ce9b1deb8391582cefce9fc42884f9504769
rayalva407 <rayalva407@gmail.com>
2021-01-18 21:08:18 -0700

    adds style and digital clock

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const secondsHand = document.querySelector('.sec-hand')
 const minutesHand = document.querySelector('.min-hand')
 const hoursHand = document.querySelector('.hour-hand')
+const digital = document.querySelector('.digital')
 
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
+  const minutes = ('0' + now.getMinutes()).slice(-2);
   const hours = now.getHours();
 
   const secondsDeg = ((seconds / 60) * 360) + 90;
@@ -16,6 +17,8 @@ function setDate() {
   secondsHand.style.transform = `rotate(${secondsDeg}deg)`
   minutesHand.style.transform = `rotate(${minutesDeg}deg)`
   hoursHand.style.transform = `rotate(${hoursDeg}deg)`
+
+  digital.innerHTML = hours >= 12 ? `<h1>${hours - 12}:${minutes}:${seconds} PM</h1>` : `<h1>${hours}:${minutes}:${seconds} AM</h1>`
 
 };
 
